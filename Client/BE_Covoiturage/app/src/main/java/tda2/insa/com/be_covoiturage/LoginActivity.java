7644dbad5c6ebd55613867339455613c498b5260
r4.saurel <r4.saurel@gmail.com>
2014-12-15 16:02:59 +0000

    login server

--- Client/BE_Covoiturage/app/src/main/java/tda2/insa/com/be_covoiturage/LoginActivity.java
+++ Client/BE_Covoiturage/app/src/main/java/tda2/insa/com/be_covoiturage/LoginActivity.java
@@ -7,6 +7,7 @@ import android.app.Activity;
 import android.app.LoaderManager.LoaderCallbacks;
 import android.content.ContentResolver;
 import android.content.CursorLoader;
+import android.content.Intent;
 import android.content.Loader;
 import android.database.Cursor;
 import android.net.Uri;
@@ -26,14 +27,17 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import org.json.JSONObject;
+
 import java.util.ArrayList;
 import java.util.List;
+import com.android.volley.*;
 
 
 /**
  * A login screen that offers login via email/password.
  */
-public class LoginActivity extends Activity implements LoaderCallbacks<Cursor> {
+public class LoginActivity extends Activity/* implements LoaderCallbacks<Cursor>*/ {
 
 	/**
 	 * A dummy authentication store containing known user names and passwords.
@@ -45,7 +49,7 @@ public class LoginActivity extends Activity implements LoaderCallbacks<Cursor> {
 	/**
 	 * Keep track of the login task to ensure we can cancel it if requested.
 	 */
-	private UserLoginTask mAuthTask = null;
+	//private UserLoginTask mAuthTask = null;
 
 	// UI references.
 	private AutoCompleteTextView mEmailView;
@@ -84,10 +88,13 @@ public class LoginActivity extends Activity implements LoaderCallbacks<Cursor> {
 
 		mLoginFormView = findViewById(R.id.login_form);
 		mProgressView = findViewById(R.id.login_progress);
+
+		mEmailView.setText("whatsthepassword");
+		mPasswordView.setText("password");
 	}
 
 	private void populateAutoComplete() {
-		getLoaderManager().initLoader(0, null, this);
+		//getLoaderManager().initLoader(0, null, this);
 	}
 
 
@@ -97,9 +104,9 @@ public class LoginActivity extends Activity implements LoaderCallbacks<Cursor> {
 	 * errors are presented and no actual login attempt is made.
 	 */
 	public void attemptLogin() {
-		if (mAuthTask != null) {
+		/*if (mAuthTask != null) {
 			return;
-		}
+		}*/
 
 		// Reset errors.
 		mEmailView.setError(null);
@@ -112,25 +119,7 @@ public class LoginActivity extends Activity implements LoaderCallbacks<Cursor> {
 		boolean cancel = false;
 		View focusView = null;
 
-
-		// Check for a valid password, if the user entered one.
-		if (!TextUtils.isEmpty(password) && !isPasswordValid(password)) {
-			mPasswordView.setError(getString(R.string.error_invalid_password));
-			focusView = mPasswordView;
-			cancel = true;
-		}
-
-		// Check for a valid email address.
-		if (TextUtils.isEmpty(email)) {
-			mEmailView.setError(getString(R.string.error_field_required));
-			focusView = mEmailView;
-			cancel = true;
-		} else if (!isEmailValid(email)) {
-			mEmailView.setError(getString(R.string.error_invalid_email));
-			focusView = mEmailView;
-			cancel = true;
-		}
-
+		((TextView)findViewById(R.id.error)).setText("");
 		if (cancel) {
 			// There was an error; don't attempt login and focus the first
 			// form field with an error.
@@ -139,59 +128,73 @@ public class LoginActivity extends Activity implements LoaderCallbacks<Cursor> {
 			// Show a progress spinner, and kick off a background task to
 			// perform the user login attempt.
 			showProgress(true);
-			//Network.
+			try {
+				JSONObject obj = new JSONObject();
+				obj.put("name", email);
+				obj.put("password", password);
+
+				Network.getInstance().sendPostRequest("http://" + Network.getHost() + "/android/login", obj, new Response.Listener<JSONObject>() {
+					@Override
+					public void onResponse(JSONObject response) {
+						LoginActivity.this.loginSuccess();
+					}
+				},
+				new Response.ErrorListener() {
+					@Override
+					public void onErrorResponse(VolleyError error) {
+						LoginActivity.this.loginFailure(error.toString());
+						//LoginActivity.this.loginSuccess();
+					}
+				});
+			}
+			catch(Exception e) {
+
+			}
 			//mAuthTask = new UserLoginTask(email, password);
 			//mAuthTask.execute((Void) null);
 		}
 	}
 
-	private boolean isEmailValid(String email) {
-		//TODO: Replace this with your own logic
-		return email.contains("@");
+	public void loginSuccess() {
+		this.showProgress(false);
+		Intent intent = new Intent(this, Settings.class);
+		startActivity(intent);
 	}
 
-	private boolean isPasswordValid(String password) {
-		//TODO: Replace this with your own logic
-		return password.length() > 4;
+	public void loginFailure(String error) {
+		this.showProgress(false);
+		((TextView)findViewById(R.id.error)).setText(error);
 	}
 
 	/**
 	 * Shows the progress UI and hides the login form.
 	 */
-	@TargetApi(Build.VERSION_CODES.HONEYCOMB_MR2)
 	public void showProgress(final boolean show) {
 		// On Honeycomb MR2 we have the ViewPropertyAnimator APIs, which allow
 		// for very easy animations. If available, use these APIs to fade-in
 		// the progress spinner.
-		if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB_MR2) {
-			int shortAnimTime = getResources().getInteger(android.R.integer.config_shortAnimTime);
+		int shortAnimTime = getResources().getInteger(android.R.integer.config_shortAnimTime);
 
-			mLoginFormView.setVisibility(show ? View.GONE : View.VISIBLE);
-			mLoginFormView.animate().setDuration(shortAnimTime).alpha(
-					show ? 0 : 1).setListener(new AnimatorListenerAdapter() {
-				@Override
-				public void onAnimationEnd(Animator animation) {
-					mLoginFormView.setVisibility(show ? View.GONE : View.VISIBLE);
-				}
-			});
+		mLoginFormView.setVisibility(show ? View.GONE : View.VISIBLE);
+		mLoginFormView.animate().setDuration(shortAnimTime).alpha(
+				show ? 0 : 1).setListener(new AnimatorListenerAdapter() {
+			@Override
+			public void onAnimationEnd(Animator animation) {
+				mLoginFormView.setVisibility(show ? View.GONE : View.VISIBLE);
+			}
+		});
 
-			mProgressView.setVisibility(show ? View.VISIBLE : View.GONE);
-			mProgressView.animate().setDuration(shortAnimTime).alpha(
-					show ? 1 : 0).setListener(new AnimatorListenerAdapter() {
-				@Override
-				public void onAnimationEnd(Animator animation) {
-					mProgressView.setVisibility(show ? View.VISIBLE : View.GONE);
-				}
-			});
-		} else {
-			// The ViewPropertyAnimator APIs are not available, so simply show
-			// and hide the relevant UI components.
-			mProgressView.setVisibility(show ? View.VISIBLE : View.GONE);
-			mLoginFormView.setVisibility(show ? View.GONE : View.VISIBLE);
-		}
+		mProgressView.setVisibility(show ? View.VISIBLE : View.GONE);
+		mProgressView.animate().setDuration(shortAnimTime).alpha(
+				show ? 1 : 0).setListener(new AnimatorListenerAdapter() {
+			@Override
+			public void onAnimationEnd(Animator animation) {
+				mProgressView.setVisibility(show ? View.VISIBLE : View.GONE);
+			}
+		});
 	}
 
-	@Override
+	/*@Override
 	public Loader<Cursor> onCreateLoader(int i, Bundle bundle) {
 		return new CursorLoader(this,
 				// Retrieve data rows for the device user's 'profile' contact.
@@ -243,13 +246,13 @@ public class LoginActivity extends Activity implements LoaderCallbacks<Cursor> {
 						android.R.layout.simple_dropdown_item_1line, emailAddressCollection);
 
 		mEmailView.setAdapter(adapter);
-	}
+	}*/
 
 	/**
 	 * Represents an asynchronous login/registration task used to authenticate
 	 * the user.
 	 */
-	public class UserLoginTask extends AsyncTask<Void, Void, Boolean> {
+	/*public class UserLoginTask extends AsyncTask<Void, Void, Boolean> {
 
 		private final String mEmail;
 		private final String mPassword;
@@ -300,7 +303,7 @@ public class LoginActivity extends Activity implements LoaderCallbacks<Cursor> {
 			mAuthTask = null;
 			showProgress(false);
 		}
-	}
+	}*/
 }
 
 
