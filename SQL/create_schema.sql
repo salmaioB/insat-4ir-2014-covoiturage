caa611cf5976ee9cdebc592dcc98e5b02b8ea0be
poisot <poisot@etud.insa-toulouse.fr>
2014-12-15 20:52:03 +0000

    Changements SQL: contraintes UNIQUE + mots de passes assez longs CreateAccount OK

--- SQL/create_schema.sql
+++ SQL/create_schema.sql
@@ -1,7 +1,7 @@
 -- --------------------------------------------------------
--- Hôte:                         127.0.0.1
--- Version du serveur:           5.5.5-10.0.15-MariaDB - mariadb.org binary distribution
--- Serveur OS:                   Win64
+-- Host:                         felix-host.ddns.net
+-- Server version:               5.5.5-10.0.15-MariaDB - mariadb.org binary distribution
+-- Server OS:                    Win64
 -- HeidiSQL Version:             8.3.0.4694
 -- --------------------------------------------------------
 
@@ -10,25 +10,29 @@
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 
--- Export de la structure de la base pour covoitsopra
-CREATE DATABASE `covoitsopra` /*!40100 DEFAULT CHARACTER SET utf8 */;
+-- Dumping database structure for covoitsopra
+DROP DATABASE IF EXISTS `covoitsopra`;
+CREATE DATABASE IF NOT EXISTS `covoitsopra` /*!40100 DEFAULT CHARACTER SET utf8 */;
 USE `covoitsopra`;
 
 
--- Export de la structure de table covoitsopra. admin
+-- Dumping structure for table covoitsopra.admin
+DROP TABLE IF EXISTS `admin`;
 CREATE TABLE IF NOT EXISTS `admin` (
   `IdAdmin` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `LastName` varchar(50) NOT NULL,
   `FirstName` varchar(50) NOT NULL,
   `Login` varchar(50) NOT NULL,
-  `Password` varchar(50) NOT NULL,
-  PRIMARY KEY (`IdAdmin`)
+  `Password` varchar(60) NOT NULL,
+  PRIMARY KEY (`IdAdmin`),
+  UNIQUE KEY `Login` (`Login`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
--- L'exportation de données n'été pas sélectionné.
+-- Data exporting was unselected.
 
 
--- Export de la structure de table covoitsopra. city
+-- Dumping structure for table covoitsopra.city
+DROP TABLE IF EXISTS `city`;
 CREATE TABLE IF NOT EXISTS `city` (
   `IdCity` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `CityName` varchar(50) NOT NULL,
@@ -36,10 +40,11 @@ CREATE TABLE IF NOT EXISTS `city` (
   PRIMARY KEY (`IdCity`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
--- L'exportation de données n'été pas sélectionné.
+-- Data exporting was unselected.
 
 
--- Export de la structure de table covoitsopra. place
+-- Dumping structure for table covoitsopra.place
+DROP TABLE IF EXISTS `place`;
 CREATE TABLE IF NOT EXISTS `place` (
   `IdPlace` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `PlaceName` varchar(50) NOT NULL DEFAULT '0',
@@ -47,10 +52,11 @@ CREATE TABLE IF NOT EXISTS `place` (
   PRIMARY KEY (`IdPlace`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
--- L'exportation de données n'été pas sélectionné.
+-- Data exporting was unselected.
 
 
--- Export de la structure de table covoitsopra. route
+-- Dumping structure for table covoitsopra.route
+DROP TABLE IF EXISTS `route`;
 CREATE TABLE IF NOT EXISTS `route` (
   `IdUser` int(10) unsigned NOT NULL,
   `Day` enum('Monday','Tuesday','Wednesday','Thursday','Friday','Saturday','Sunday') NOT NULL,
@@ -60,27 +66,29 @@ CREATE TABLE IF NOT EXISTS `route` (
   CONSTRAINT `FK_route_user` FOREIGN KEY (`IdUser`) REFERENCES `user` (`IdUser`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
--- L'exportation de données n'été pas sélectionné.
+-- Data exporting was unselected.
 
 
--- Export de la structure de table covoitsopra. user
+-- Dumping structure for table covoitsopra.user
+DROP TABLE IF EXISTS `user`;
 CREATE TABLE IF NOT EXISTS `user` (
   `IdUser` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `MailAddress` varchar(100) NOT NULL,
   `LastName` varchar(50) NOT NULL,
   `FirstName` varchar(50) NOT NULL,
-  `Password` varchar(50) NOT NULL,
+  `Password` varchar(60) NOT NULL,
   `Driver` enum('Y','N') NOT NULL,
-  `IdCity` int(10) unsigned,
-  `IdPlace` int(10) unsigned,
+  `IdCity` int(10) unsigned DEFAULT NULL,
+  `IdPlace` int(10) unsigned DEFAULT NULL,
   PRIMARY KEY (`IdUser`),
+  UNIQUE KEY `MailAddress` (`MailAddress`),
   KEY `FK_user_city` (`IdCity`),
   KEY `FK_user_place` (`IdPlace`),
   CONSTRAINT `FK_user_city` FOREIGN KEY (`IdCity`) REFERENCES `city` (`IdCity`),
   CONSTRAINT `FK_user_place` FOREIGN KEY (`IdPlace`) REFERENCES `place` (`IdPlace`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
--- L'exportation de données n'été pas sélectionné.
+-- Data exporting was unselected.
 /*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
 /*!40014 SET FOREIGN_KEY_CHECKS=IF(@OLD_FOREIGN_KEY_CHECKS IS NULL, 1, @OLD_FOREIGN_KEY_CHECKS) */;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
--- Server/src/covoit/User.java
+++ Server/src/covoit/User.java
@@ -5,6 +5,7 @@
 package covoit;
 
 import java.sql.*;
+import covoit.sql.Conn;
 
 /** User account, with their personnal informations. */
 public class User
@@ -12,40 +13,67 @@ public class User
    public static User load(String name) throws SQLException
    {
       User r = new User();
-      String req = "SELECT Password, Driver FROM user WHERE MailAddress = ?";
-      ResultSet u = covoit.sql.Conn.execute(req, name);
+      
+      String q = "SELECT Password, FirstName, LastName, Driver "+
+                 "FROM user "+
+                 "WHERE MailAddress = ?";
+      PreparedStatement st = Conn.prepare(q);
+      st.setString(1, name);
+      ResultSet u = st.executeQuery();
       
       if (!u.next())
          return null; //got 0 rows
          
       r.name = name;
       r.passwd = u.getString("Password");
+      r.firstName = u.getString("FirstName");
+      r.lastName = u.getString("LastName");
       r.driver = u.getString("Driver").equals("Y");
       
+      
       u.close();
+      st.close();
       return r;
    }
    
-   public static User create(String name, String pwdHash) throws SQLException
+   public static User create(String name, String pwdHash, 
+                             String firstName, String lastName, boolean driver
+                             ) throws SQLException
    {
       User u = new User();
       u.name = name;
       u.passwd = pwdHash;
+      u.firstName = firstName;
+      u.lastName = lastName;
+      u.driver = driver;
       
-      String req = "INSERT INTO user (MailAddress, Password) VALUES (?, ?)";
-      ResultSet r = covoit.sql.Conn.execute(req, name, pwdHash);
-      r.close();
+      String req = "INSERT INTO user "+
+            "       (MailAddress, Password, FirstName, LastName, Driver) "+
+            "VALUES (?, ?, ?, ?, ?)";
+      PreparedStatement st = Conn.prepare(req);
+      st.setString(1, name);
+      st.setString(2, pwdHash);
+      st.setString(3, firstName);
+      st.setString(4, lastName);
+      st.setString(5, (driver) ? ("Y") : ("N"));
+      st.execute();
+      st.close();
       
       return u;
    }
 
    /** base64(bcrypt([password])) */
    public String getPassword()      {return passwd;}
+   public String getFirstName()     {return firstName;}
+   public String getLastName()      {return lastName;}
+   public boolean isDriver()        {return driver;}
    
 /******************************************************************************/
 
    private String name; // aussi l'adresse email ?
    private String passwd; 
+   private String firstName;
+   private String lastName;
    private boolean driver; // Vrai si la personne préfère conduire elle-même.
    
    private User()    {}
