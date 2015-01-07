9f2501096033756a6ddca1a0b905cd7bc3e61b23
r4.saurel <r4.saurel@gmail.com>
2015-01-07 09:32:32 +0000

    [android] Modif classe User

--- Client/BE_Covoiturage/app/src/main/java/tda2/insa/com/be_covoiturage/User.java
+++ Client/BE_Covoiturage/app/src/main/java/tda2/insa/com/be_covoiturage/User.java
@@ -4,15 +4,50 @@ import android.util.Log;
 
 import org.json.JSONObject;
 
+import java.util.ArrayList;
+
 /**
  * Created by remi on 04/01/15.
  */
 public class User {
 	private AuthToken _authToken;
+	private ArrayList<Route> _routes;
+	private String _firstName, _lastName;
+	private boolean _isDriver;
 
 	User(AuthToken authToken, JSONObject userInfo) {
 		_authToken = authToken;
+		_routes = new ArrayList<>();
+
+		_firstName = "";
+		_lastName = "";
+		_isDriver = false;
+
 		Log.e("Creating user with info", userInfo.toString());
+	}
+
+	public ArrayList<Route> getRoutes() {
+		return _routes;
+	}
+
+	public void addRoute(Route r) {
+		_routes.add(r);
+	}
+
+	public void removeRoute(Route r) {
+		_routes.remove(r);
+	}
+
+	public boolean isDriver() {
+		return _isDriver;
+	}
+
+	public String getFirstName() {
+		return _firstName;
+	}
+
+	public String getLastName() {
+		return _lastName;
 	}
 
 	public AuthToken getAuthToken() {
