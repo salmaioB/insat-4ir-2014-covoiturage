ff507a06f2a54b144252e5359bd499f83e0d46b5
phuthanh.nguyen215 <phuthanh.nguyen215@934ec9f7-9ced-5dbd-dda4-3c09657c2280>
2015-01-09 16:14:07 +0000

    Commit

--- Client/BE_Covoiturage/app/src/main/java/tda2/insa/com/be_covoiturage/EditSetting.java
+++ Client/BE_Covoiturage/app/src/main/java/tda2/insa/com/be_covoiturage/EditSetting.java
@@ -2,11 +2,14 @@ package tda2.insa.com.be_covoiturage;
 
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
 
-public class EditSetting extends ActionBarActivity {
+public class EditSetting extends ActionBarActivity implements Button.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,5 +38,12 @@ public class EditSetting extends ActionBarActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (v.getId() == R.id.button){
+            //Settings
+        }
     }
 }
