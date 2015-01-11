c3b1b4028de49c165dec34f0e2ed2078cf4fb786
r4.saurel@gmail.com <r4.saurel@gmail.com@934ec9f7-9ced-5dbd-dda4-3c09657c2280>
2015-01-11 20:10:06 +0000

    [serveur] ajout fichier oublié

---
+++ trunk/Server/src/covoit/Workplaces.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package covoit;
+
+import covoit.sql.Conn;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import javax.json.*;
+
+/**
+ *
+ * @author remi
+ */
+public class Workplaces {
+	public static JsonArrayBuilder getWorplaces() throws SQLException {
+		JsonArrayBuilder array = Json.createArrayBuilder();
+        String q = "SELECT IdPlace, PlaceName, PlaceAddress "
+                + "FROM place ";
+		
+        PreparedStatement st = Conn.prepare(q);
+        ResultSet u = st.executeQuery();
+		
+		while(u.next()) {
+            JsonObjectBuilder builder = Json.createObjectBuilder();
+			builder.add("id", u.getInt("IdPlace"));
+			builder.add("name", u.getString("PlaceName"));
+			builder.add("address", u.getString("PlaceAddress"));
+			
+			array.add(builder);
+        }
+		
+		return array;
+	}
+}
