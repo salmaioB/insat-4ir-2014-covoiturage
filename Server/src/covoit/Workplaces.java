af0ee241688d7caa71858e78d65e8a30e5ba02cb
chevauxenforce <chevauxenforce@934ec9f7-9ced-5dbd-dda4-3c09657c2280>
2015-01-13 15:19:58 +0000

    doSearchRoutes

--- Server/src/covoit/Workplaces.java
+++ Server/src/covoit/Workplaces.java
@@ -16,24 +16,24 @@ import javax.json.*;
  * @author remi
  */
 public class Workplaces {
-    
-	public static JsonArrayBuilder getWorkplaces() throws SQLException {
-		JsonArrayBuilder array = Json.createArrayBuilder();
+
+    public static JsonArrayBuilder getWorkplaces() throws SQLException {
+        JsonArrayBuilder array = Json.createArrayBuilder();
         String q = "SELECT IdPlace, PlaceName, PlaceAddress "
                 + "FROM place ";
-		
+
         PreparedStatement st = Conn.prepare(q);
         ResultSet u = st.executeQuery();
-		
-		while(u.next()) {
+
+        while (u.next()) {
             JsonObjectBuilder builder = Json.createObjectBuilder();
-			builder.add("id", u.getInt("IdPlace"));
-			builder.add("name", u.getString("PlaceName"));
-			builder.add("address", u.getString("PlaceAddress"));
-			
-			array.add(builder);
+            builder.add("id", u.getInt("IdPlace"));
+            builder.add("name", u.getString("PlaceName"));
+            builder.add("address", u.getString("PlaceAddress"));
+
+            array.add(builder);
         }
-		
-		return array;
-	}
+
+        return array;
+    }
 }
