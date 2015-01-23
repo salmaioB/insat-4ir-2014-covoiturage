49b1ac36481ae261f8e0a3a1579e3b3969f70bf9
yorrick.barnay <yorrick.barnay@gmail.com>
2015-01-23 08:25:37 +0000

    Début de code de la partie Admin

---
+++ Server/src/covoit/Admin.java
@@ -0,0 +1,102 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package covoit;
+import java.sql.*;
+import covoit.sql.Conn;
+
+/**
+ *
+ * @author yorrickbarnay
+ */
+public class Admin {
+    
+    private String name; //correspond à l'adresse email
+    private String passwd;
+    private String firstName;
+    private String lastName;
+    
+    public Admin() {
+    }
+    
+    public String getName() {return name;}
+    public String getPassword() {return passwd;}
+    public String getFirstName() {return firstName;}
+    public String getLastName() {return lastName;}
+    
+    public static void updatePassword(String mailAddr, String password) throws SQLException {
+        String q = "UPDATE user SET Password = ? WHERE MailAddress = ?;";
+        PreparedStatement st = Conn.prepare(q);
+        st.setString(1, password);
+        st.setString(2, mailAddr);
+        st.execute();
+        st.close();
+    }
+    
+    public static Admin load(String name) throws SQLException {
+        Admin r = new Admin();
+
+            String q = "SELECT IdAdmin, Password, FirstName, LastName "
+                            + "FROM admin"
+                            + "WHERE MailAddress = ? ;";
+            
+            PreparedStatement st = Conn.prepare(q);
+            st.setString(1, name);
+            ResultSet u = st.executeQuery();
+
+            if (!u.next()) {
+                    return null; //got 0 rows
+            }
+            r.name = name;
+            r.passwd = u.getString("Password");
+            r.firstName = u.getString("FirstName");
+            r.lastName = u.getString("LastName");
+            int iduser = u.getInt("IdAdmin");
+
+            u.close();
+            st.close();
+            
+        return r;
+    }
+    
+    public static void addPlace(String placeName, String placeAddress) throws SQLException {
+
+        String q = "SELECT * FROM place WHERE PlaceName = ? OR PlaceAddress = ?;";
+        PreparedStatement st = Conn.prepare(q);
+        st.setString(1, placeName);
+        st.setString(2, placeAddress);
+
+        ResultSet rs = st.executeQuery();
+                
+        //si ni le nom, ni l'addresse n'ont jamais été rentré
+        if (!rs.next()) {
+            q = "INSERT INTO covoitsopra.place (`PlaceName`, `PlaceAddress`) "
+                    + "VALUES (?, ?);";
+
+            st = Conn.prepare(q);
+            st.setString(1, placeName);
+            st.setString(2, placeAddress);
+
+            st.execute();
+            st.close();
+        }
+        
+        rs.close();
+
+    }
+    
+    public static void deletePlace(String placeName, String placeAddress) throws SQLException {
+
+        String q = "DELETE FROM covoitsopra.place WHERE PlaceName = ? AND PlaceAddress = ?;";
+        PreparedStatement  st = Conn.prepare(q);
+        
+        st.setString(1, placeName);
+        st.setString(2, placeAddress);
+
+        st.execute();
+        st.close();
+
+    }
+}
